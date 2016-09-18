e197b137a1d4fc0c0882fbdecb3b70ad5e7667ef
Amir Keivan Mohtashami <akmohtashami97@gmail.com>
2016-09-19 00:12:51 +0430

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+# Django Clone - https://github.com/mohammadroghani/django-clone
+# Copyright © 2016 Mohammad Roghani <mohammad.roghani43@gmail.com>
+# Copyright © 2016 Amir Keivan Mohtashami <akmohtashami97@gmail.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from setuptools import setup
+
+setup(name='django-clone',
+		version='0.1',
+		description='Deep Copy for Django models',
+		url='http://github.com/storborg/funniest',
+		author='Mohammad Roghani, Amir Keivan Mohtashami',
+		author_email='mohammad.roghani43@gmail.com, akmohtashami97@gmail.com',
+		license='MIT',
+		packages=['django_clone'],
+		zip_safe=False)
